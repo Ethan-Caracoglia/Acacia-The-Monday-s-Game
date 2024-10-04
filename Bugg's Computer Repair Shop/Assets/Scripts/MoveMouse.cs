433dc5001cf69ff1c6c4087ac7280d7ae9f45eb4
gnk1165 <54484011+gnk1165@users.noreply.github.com>
2024-10-04 15:52:54 -0400

    Added Basic Interface

--- Bugg's Computer Repair Shop/Assets/Scripts/MoveMouse.cs
+++ Bugg's Computer Repair Shop/Assets/Scripts/MoveMouse.cs
@@ -10,6 +10,7 @@ public class MoveMouse : MonoBehaviour
     float height;
     float width;
 
+    private ContactFilter2D c = new ContactFilter2D();
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
@@ -19,20 +20,16 @@ public class MoveMouse : MonoBehaviour
 
     public void UpdateMouse(InputAction.CallbackContext ctx)
     {
-        // Vector that points to screen center from mouse pos zero
-        Vector3 halfScreenSize = new Vector3(Screen.width / 2, Screen.height / 2, 0);
         Vector2 mousePos = ctx.ReadValue<Vector2>();
-        float hWidth = width / 2;
-        float hHeight = height / 2;
 
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10)); 
-
-         /* (Debug.Log(
-            $"Mouse Position = {mousePos}\n" +
-            $"Mouse Position in units = {mousePos / PIXELS_TO_UNITY_UNITY_UNITS_RATIO}\n" +
-            $"HalfScreen vector = {halfScreenSize}\n" +
-            $"Cursor Position = {transform.position}\n" + 
-            $"Width and height = {width}, {height}");
-            */
     }
+
+    public void GetMouseDown(InputAction.CallbackContext ctx)
+    {
+        Collider2D[] results = new Collider2D[] { };
+        Physics2D.OverlapPoint(transform.position, c.NoFilter(), results);
+
+    }
+
 }
