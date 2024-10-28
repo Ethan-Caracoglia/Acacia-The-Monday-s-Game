d372d906941adf91555d482bc3f0f56a2a2c97df
rwp3328 <114249591+rwp3328@users.noreply.github.com>
2024-10-28 15:54:47 -0400

    Updated script for score system

--- Bugg's Computer Repair Shop/Assets/Scripts/ScoreManager.cs
+++ Bugg's Computer Repair Shop/Assets/Scripts/ScoreManager.cs
@@ -16,17 +16,26 @@ public class ScoreManager : MonoBehaviour
     UImanager objectives;
 
     [SerializeField]
-    int caseRemovedScore;
-    int iceMeltedScore;
-    int partsReassembledScore;
+    int caseRemovedScore = -1;
+    [SerializeField]
+    int iceMeltedScore = -1;
+    [SerializeField]
+    int partsReassembledScore = -1;
 
     [SerializeField]
     float timer = 0;
+    int currentObjective = 0;
+
+    int caseRemovedDelay;
+    int iceMeltedDelay;
+    int partsReassembledDelay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        caseRemovedDelay = 1;
+        iceMeltedDelay = 2;
+        partsReassembledDelay = 1;
     }
 
     // Update is called once per frame
@@ -35,10 +44,45 @@ public class ScoreManager : MonoBehaviour
         caseRemoved = objectives.CaseRemoved;
         iceMelted = objectives.IceMelted;
         partsReassembled = objectives.PartsReassembled;
-        caseRemovedScore = ScoreTimer(caseRemoved);
+        if (currentObjective == 0)
+        {
+            if (caseRemovedScore == -1)
+            {
+                caseRemovedScore = ScoreTimer(caseRemoved, caseRemovedDelay);
+            }
+            else
+            {
+                timer = 0;
+                currentObjective = 1;
+            }
+        }
+        if (currentObjective == 1)
+        {
+            if (iceMeltedScore == -1)
+            {
+                iceMeltedScore = ScoreTimer(iceMelted, iceMeltedDelay);
+            }
+            else
+            {
+                timer = 0;
+                currentObjective = 2;
+            }
+        }
+        if (currentObjective == 2)
+        {
+            if (partsReassembledScore == -1)
+            {
+                partsReassembledScore = ScoreTimer(partsReassembled, partsReassembledDelay);
+            }
+            else
+            {
+                timer = 0;
+                currentObjective = 3;
+            }
+        }
     }
 
-    public int ScoreTimer(bool objectiveDone)
+    public int ScoreTimer(bool objectiveDone, int timeDelay)
     {
         int score = -1;
         if (objectiveDone == false)
@@ -47,27 +91,27 @@ public class ScoreManager : MonoBehaviour
         }
         if (objectiveDone == true)
         {
-            if (timer <= 1)
+            if (timer <= (1*timeDelay))
             {
                 score = 5;
             }
-            else if (timer > 1 && timer <= 2)
+            else if (timer > (1 * timeDelay) && timer <= (2 * timeDelay))
             {
                 score = 4;
             }
-            else if (timer > 2 && timer <= 3)
+            else if (timer > (2 * timeDelay) && timer <= (3 * timeDelay))
             {
                 score = 3;
             }
-            else if (timer > 3 && timer <= 4)
+            else if (timer > (3 * timeDelay) && timer <= (4 * timeDelay))
             {
                 score = 2;
             }
-            else if (timer > 4 && timer <= 5)
+            else if (timer > (41 * timeDelay) && timer <= (5 * timeDelay))
             {
                 score = 1;
             }
-            else if (timer > 5)
+            else if (timer > (5 * timeDelay))
             {
                 score = 0;
             }
