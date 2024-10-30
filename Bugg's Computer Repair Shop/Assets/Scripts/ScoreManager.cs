7ebc1e60999877d0fed3a6510c55217ddc076e7f
rwp3328 <114249591+rwp3328@users.noreply.github.com>
2024-10-30 15:56:55 -0400

    Made progress on score manager
    Technically can work. Current change in code is unfinished, so does not work correctly

--- Bugg's Computer Repair Shop/Assets/Scripts/ScoreManager.cs
+++ Bugg's Computer Repair Shop/Assets/Scripts/ScoreManager.cs
@@ -1,54 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Objectives
-    [SerializeField]
-    bool caseRemoved = false;
-    [SerializeField]
-    bool iceMelted = false;
-    [SerializeField]
-    bool partsReassembled = false;
-
     [SerializeField]
     UImanager objectives;
 
+    // Score for each objective
     [SerializeField]
     int caseRemovedScore = -1;
     [SerializeField]
     int iceMeltedScore = -1;
     [SerializeField]
     int partsReassembledScore = -1;
+    [SerializeField]
+    int totalScore = 0;
+    int totalPossibleScore = 0;
 
     [SerializeField]
+    List<int> objectivesScore;
+    [SerializeField]
+    List<bool> objectivesProgress;
+    [SerializeField]
+    int objectivesAmount;
+
+
+
+    public int CaseRemovedScore
+    {
+        get
+        {
+            return caseRemovedScore;
+        }
+    }
+    public int IceMeltedScore
+    {
+        get
+        {
+            return caseRemovedScore;
+        }
+    }
+    public int PartsReassembledScore
+    {
+        get
+        {
+            return caseRemovedScore;
+        }
+    }
+
+    // Keeps track of time taken for an objectove
+    [SerializeField]
     float timer = 0;
+
+    // Tracks onjective progress
+    [SerializeField]
     int currentObjective = 0;
 
+    // Records if all objectives are done
+    [SerializeField]
+    bool objectivesDone;
+
+    // Delay to increase time conversion for score
     int caseRemovedDelay;
     int iceMeltedDelay;
     int partsReassembledDelay;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
         caseRemovedDelay = 1;
-        iceMeltedDelay = 2;
+        iceMeltedDelay = 4;
         partsReassembledDelay = 1;
+        objectivesScore = new List<int>(objectivesAmount);
+        for (int i = 0; i < objectivesAmount; i++)
+        {
+            objectivesProgress.Add(false);
+        }
+        for (int i = 0; i < objectivesAmount; i++)
+        {
+            objectivesScore.Add(-1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        caseRemoved = objectives.CaseRemoved;
-        iceMelted = objectives.IceMelted;
-        partsReassembled = objectives.PartsReassembled;
+        objectivesProgress[0] = objectives.CaseRemoved;
+        objectivesProgress[1] = objectives.IceMelted;
+        objectivesProgress[2] = objectives.PartsReassembled;
+        /*
         if (currentObjective == 0)
         {
             if (caseRemovedScore == -1)
             {
-                caseRemovedScore = ScoreTimer(caseRemoved, caseRemovedDelay);
+                caseRemovedScore = ScoreTimer(objectivesProgress[0], caseRemovedDelay);
             }
             else
             {
@@ -60,7 +110,7 @@ public class ScoreManager : MonoBehaviour
         {
             if (iceMeltedScore == -1)
             {
-                iceMeltedScore = ScoreTimer(iceMelted, iceMeltedDelay);
+                iceMeltedScore = ScoreTimer(objectivesProgress[1], iceMeltedDelay);
             }
             else
             {
@@ -72,13 +122,44 @@ public class ScoreManager : MonoBehaviour
         {
             if (partsReassembledScore == -1)
             {
-                partsReassembledScore = ScoreTimer(partsReassembled, partsReassembledDelay);
+                partsReassembledScore = ScoreTimer(objectivesProgress[2], partsReassembledDelay);
             }
             else
             {
                 timer = 0;
                 currentObjective = 3;
+                objectivesDone = true;
             }
+        }
+        */
+        if (currentObjective < objectivesAmount)
+        {
+            if (objectivesScore[currentObjective] == -1)
+            {
+                objectivesScore[currentObjective] = ScoreTimer(objectivesProgress[2], 1);
+            }
+            else
+            {
+                timer = 0;
+                currentObjective++;
+            }
+        }
+        else
+        {
+            objectivesDone = true;
+        }
+
+
+        if (objectivesDone == true)
+        {
+            totalScore = caseRemovedScore + iceMeltedScore + partsReassembledScore;
+            totalPossibleScore = 5 * objectivesAmount;
+            scoreDisplay.text = $"Total Score = {totalScore}/{totalPossibleScore}";
+        }
+        if (objectivesDone == false)
+        {
+            totalPossibleScore = 5 * objectivesAmount;
+            scoreDisplay.text = $"Total Score = ?/{totalPossibleScore}";
         }
     }
 
@@ -91,27 +172,27 @@ public class ScoreManager : MonoBehaviour
         }
         if (objectiveDone == true)
         {
-            if (timer <= (1*timeDelay))
+            if (timer <= (2*timeDelay))
             {
                 score = 5;
             }
-            else if (timer > (1 * timeDelay) && timer <= (2 * timeDelay))
+            else if (timer > (2 * timeDelay) && timer <= (3 * timeDelay))
             {
                 score = 4;
             }
-            else if (timer > (2 * timeDelay) && timer <= (3 * timeDelay))
+            else if (timer > (3 * timeDelay) && timer <= (4 * timeDelay))
             {
                 score = 3;
             }
-            else if (timer > (3 * timeDelay) && timer <= (4 * timeDelay))
+            else if (timer > (4 * timeDelay) && timer <= (5 * timeDelay))
             {
                 score = 2;
             }
-            else if (timer > (41 * timeDelay) && timer <= (5 * timeDelay))
+            else if (timer > (5 * timeDelay) && timer <= (6 * timeDelay))
             {
                 score = 1;
             }
-            else if (timer > (5 * timeDelay))
+            else if (timer > (6 * timeDelay))
             {
                 score = 0;
             }
