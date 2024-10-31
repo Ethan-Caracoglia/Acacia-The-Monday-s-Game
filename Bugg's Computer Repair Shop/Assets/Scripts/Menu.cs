0015b973986ec12bf49f9d676e839b768769fac9
spoopyboi3 <113314005+spoopyboi3@users.noreply.github.com>
2024-10-31 14:03:23 -0400

    scene transitions finished

--- Bugg's Computer Repair Shop/Assets/Scripts/Menu.cs
+++ Bugg's Computer Repair Shop/Assets/Scripts/Menu.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] RectTransform fader;
 
+    private void Start()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setEase(LeanTweenType.easeInOutExpo).setOnComplete(() =>
+        {
+            fader.gameObject.SetActive(false);
+        });
+    }
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);
+        fader.gameObject.SetActive(true) ;
+        LeanTween.scale(fader, Vector3.zero, 0);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setEase(LeanTweenType.easeInOutExpo).setOnComplete(() =>
+        {
+            Invoke("LoadGame", 0.5f);
+        });
+        
     }
 
     public void OnQuitButton()
@@ -16,4 +33,8 @@ public class Menu : MonoBehaviour
         Application.Quit();
     }
 
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
