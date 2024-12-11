accd877a6b5782ebfbe26248e2524670f2679258
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2024-12-10 20:45:55 -0500

    Multi Ice added

--- Bugg's Computer Repair Shop/Assets/Scripts/Melt.cs
+++ Bugg's Computer Repair Shop/Assets/Scripts/Melt.cs
@@ -11,34 +11,23 @@ public class Melt : MonoBehaviour
     #region public
     // This should NOT be how we do this
     public SpriteRenderer spr;
+    public List<GameObject> iceCubes;
     #endregion
 
     #region private
-    [SerializeField] float meltRate = 0.4f;
-    [SerializeField] float minSize = 0.3f;
     [SerializeField] BasicWin b;
     private bool isMouseDown;
     #endregion
     #endregion
 
     #region Methods
-    #region public
-    public void Melting()
-    {
-        Debug.Log("melt");
-        //check how big the gameobject is, if its not too tiny to reasonably click on, then start scaling it down
-        float meltAmount = meltRate * Time.deltaTime;
-        transform.localScale -= new Vector3(meltAmount, meltAmount, 0);
-    }
-    #endregion
 
     #region private
     private void Update()
     {
-        if (transform.localScale.x <= minSize || transform.localScale.y <= minSize)
+        if (iceCubes.Count == 0)
         {
             b.updateIceCount();
-            Destroy(gameObject);
         }
     }
     #endregion
